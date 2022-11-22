e389c548f0c861b7d90ccfe6a7b5b5f01bb87981
aarneharju <35223549+aarneharju@users.noreply.github.com>
2022-11-22 21:37:59 +0200

    3.2 Done

--- index.js
+++ index.js
@@ -2,38 +2,46 @@ const express = require('express');
 const app = express();
 
 let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
 ];
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+  response.json(persons);
 });
 
 app.get('/test', (request, response) => {
-    response.send('Hello hello. This is the test folder.');
-})
+  response.send('Hello hello. This is the test folder.');
+});
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people.</p>
+    <p>${new Date()}</p>`
+  );
+  response.send(``);
+});
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
