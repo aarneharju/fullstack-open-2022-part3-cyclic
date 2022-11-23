01e8d83c647004310d174a37d551928b465c1d49
aarneharju <35223549+aarneharju@users.noreply.github.com>
2022-11-23 22:10:41 +0200

    3.4 done

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 });
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+  response.status(204).end();
+});
+
 app.get('/test', (request, response) => {
   response.send('Hello hello. This is the test folder.');
 });
