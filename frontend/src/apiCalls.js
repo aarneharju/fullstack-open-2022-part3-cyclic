6ad970c5bfb0c36090a180558e96f48206a00e4d
aarneharju <35223549+aarneharju@users.noreply.github.com>
2023-03-18 13:33:55 +0200

    Tidying up empty lines etc.

--- frontend/src/apiCalls.js
+++ frontend/src/apiCalls.js
@@ -3,24 +3,21 @@ import axios from 'axios';
 // const databaseUrl = 'http://localhost:3001/api/persons';
 const baseUrl = '/api/persons';
 
-const getAllPersons = () =>
-    axios.get(baseUrl)
-        .then(response => response.data)
+const getAllPersons = () => axios.get(baseUrl).then(response => response.data);
 
 const addPerson = person =>
-    axios.post(baseUrl, person)
-        .then(response => {
-
-            return response.data
-        })
+  axios.post(baseUrl, person).then(response => {
+    return response.data;
+  });
 
 const updatePerson = (personID, newObject) => {
-    return axios.put(`${baseUrl}/${personID}`, newObject)
-        .then(response => response.data)
-}
+  return axios
+    .put(`${baseUrl}/${personID}`, newObject)
+    .then(response => response.data);
+};
 
 const deletePerson = personID => {
-    return axios.delete(`${baseUrl}/${personID}`)
-}
+  return axios.delete(`${baseUrl}/${personID}`);
+};
 
 export default { getAllPersons, addPerson, updatePerson, deletePerson };
